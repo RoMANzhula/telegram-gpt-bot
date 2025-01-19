8ad2afbbfcfa2139539456a27e89747aa985199c
RoMANzhula <roman1985al@ukr.net>
2025-01-19 22:39:05 +0200

    Add TelegramGptBotApplication - main class

---
+++ src/main/java/org/romanzhula/telegram_gpt_bot/TelegramGptBotApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.telegram_gpt_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramGptBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramGptBotApplication.class, args);
+	}
+
+}
