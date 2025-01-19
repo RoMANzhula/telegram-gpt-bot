211ca934e551862b35e783cfef3cc5746403c99b
RoMANzhula <roman1985al@ukr.net>
2025-01-19 22:39:20 +0200

    Add TelegramBotConfig

---
+++ src/main/java/org/romanzhula/telegram_gpt_bot/configurations/TelegramBotConfig.java
@@ -0,0 +1,11 @@
+package org.romanzhula.telegram_gpt_bot.configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class TelegramBotConfig {
+
+
+
+}
