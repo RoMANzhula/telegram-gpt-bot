418e483ce5553b6e582a6da1ff824a9ec929b110
RoMANzhula <roman1985al@ukr.net>
2025-01-20 20:38:16 +0200

    Add BotSettings

---
+++ src/main/java/org/romanzhula/telegram_gpt_bot/telegram/BotSettings.java
@@ -0,0 +1,24 @@
+package org.romanzhula.telegram_gpt_bot.telegram;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class BotSettings {
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Value("${telegram.bot.name}")
+    private String botName;
+
+
+    public String getBotToken() {
+        return botToken;
+    }
+
+    public String getBotName() {
+        return botName;
+    }
+}
