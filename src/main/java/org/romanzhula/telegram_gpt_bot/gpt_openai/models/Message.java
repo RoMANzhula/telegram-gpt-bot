86b2ac0072f3b091a6dabe969fd0a2001fee4cc5
RoMANzhula <roman1985al@ukr.net>
2025-01-20 22:50:50 +0200

    Up Message - record to class

--- src/main/java/org/romanzhula/telegram_gpt_bot/gpt_openai/models/Message.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/gpt_openai/models/Message.java
@@ -1,9 +1,17 @@
 package org.romanzhula.telegram_gpt_bot.gpt_openai.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
+public class Message {
 
-public record Message(
-        @JsonProperty("role") String role,
-        @JsonProperty("content") String content
-) {}
+    @JsonProperty("role")
+    private String role;
+
+    @JsonProperty("content")
+    private String content;
+
+}
