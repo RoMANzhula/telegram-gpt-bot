2064452d7a7fad9ed4078fa6707c37c54b438067
RoMANzhula <roman1985al@ukr.net>
2025-01-20 20:37:34 +0200

    Up TelegramBot - add gpt

--- src/main/java/org/romanzhula/telegram_gpt_bot/TelegramBot.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/TelegramBot.java
@@ -1,5 +1,9 @@
 package org.romanzhula.telegram_gpt_bot;
 
+import org.romanzhula.telegram_gpt_bot.gpt_openai.GptClient;
+import org.romanzhula.telegram_gpt_bot.gpt_openai.models.ChatCompletion;
+import org.romanzhula.telegram_gpt_bot.telegram.BotSettings;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -7,15 +11,25 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
+@Component
 public class TelegramBot extends TelegramLongPollingBot {
 
-    public TelegramBot(DefaultBotOptions options, String botToken) {
-        super(options, botToken);
+    private final BotSettings botSettings;
+    private final GptClient gptClient;
+
+    public TelegramBot(
+            DefaultBotOptions options,
+            BotSettings botSettings,
+            GptClient gptClient
+    ) {
+        super(options, botSettings.getBotToken());
+        this.botSettings = botSettings;
+        this.gptClient = gptClient;
     }
 
     @Override
     public String getBotUsername() {
-        return "";
+        return botSettings.getBotName();
     }
 
     @Override
@@ -24,7 +38,10 @@ public class TelegramBot extends TelegramLongPollingBot {
             String text = update.getMessage().getText();
             Long chatId = update.getMessage().getChatId();
 
-            SendMessage sendMessage = new SendMessage(chatId.toString(), text);
+            ChatCompletion chatCompletionResponse = gptClient.createChatCompletion(text);
+            String textResponse = chatCompletionResponse.choises().get(0).message().content();
+
+            SendMessage sendMessage = new SendMessage(chatId.toString(), textResponse);
 
             try {
                 sendApiMethod(sendMessage);
