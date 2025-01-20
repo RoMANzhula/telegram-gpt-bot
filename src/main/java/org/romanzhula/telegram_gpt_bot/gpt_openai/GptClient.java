20e04de0785216d246db95368d9f7bd8969d4701
RoMANzhula <roman1985al@ukr.net>
2025-01-20 22:56:21 +0200

    Up GptClient - add ChatCompletionRequest

--- src/main/java/org/romanzhula/telegram_gpt_bot/gpt_openai/GptClient.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/gpt_openai/GptClient.java
@@ -1,9 +1,11 @@
 package org.romanzhula.telegram_gpt_bot.gpt_openai;
 
 import org.romanzhula.telegram_gpt_bot.gpt_openai.models.ChatCompletion;
+import org.romanzhula.telegram_gpt_bot.gpt_openai.requests.ChatCompletionRequest;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
 
 
 @Component
@@ -12,38 +14,23 @@ public class GptClient {
     private final String token;
     private final RestTemplate restTemplate;
 
-    public GptClient(String token, RestTemplate restTemplate) {
+    public GptClient(
+            String token,
+            RestTemplate restTemplate
+    ) {
         this.token = token;
         this.restTemplate = restTemplate;
     }
 
 
-    public ChatCompletion createChatCompletion(String message) {
+    public ChatCompletion createChatCompletion(ChatCompletionRequest request) {
         String url = "https://api.openai.com/v1/chat/completions";
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set("Authorization", "Bearer " + token);
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
-        String requestBody = """
-                {
-                    "model": "gpt-3.5-turbo",
-                    "messages": [
-                      {
-                        "role": "developer",
-                        "content": "You are a helpful assistant."
-                      },
-                      {
-                        "role": "user",
-                        "content": "%s"
-                      }
-                    ],
-                    "max_tokens": 100
-                  }
-                """.formatted(message)
-        ;
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(requestBody, httpHeaders);
+        HttpEntity<ChatCompletionRequest> httpEntity = new HttpEntity<>(request, httpHeaders);
 
         ResponseEntity<ChatCompletion> responseEntity = restTemplate.exchange(
                 url,
