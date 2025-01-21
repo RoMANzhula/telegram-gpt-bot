14e2af17b3da79b7a71afad5457caaef1f748585
RoMANzhula <roman1985al@ukr.net>
2025-01-21 18:09:06 +0200

    Up GptService - store in local map

--- src/main/java/org/romanzhula/telegram_gpt_bot/gpt_openai/services/GptService.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/gpt_openai/services/GptService.java
@@ -4,6 +4,7 @@ import jakarta.annotation.Nonnull;
 import lombok.AllArgsConstructor;
 import org.romanzhula.telegram_gpt_bot.gpt_openai.GptClient;
 import org.romanzhula.telegram_gpt_bot.gpt_openai.models.ChatCompletion;
+import org.romanzhula.telegram_gpt_bot.gpt_openai.models.GptChatHistory;
 import org.romanzhula.telegram_gpt_bot.gpt_openai.models.Message;
 import org.romanzhula.telegram_gpt_bot.gpt_openai.requests.ChatCompletionRequest;
 import org.springframework.stereotype.Service;
@@ -15,25 +16,33 @@ import java.util.List;
 public class GptService {
 
     private final GptClient gptClient;
+    private final GptChatHistoryService gptChatHistoryService;
 
     @Nonnull
     public String getGptResponse(
-        Long userId,
+        Long userChatId,
         String userText
     ) {
+        gptChatHistoryService.createChatIdHistory(userChatId);
+
+        GptChatHistory gptChatHistory = gptChatHistoryService.updateChatIdHistory(
+                userChatId,
+                Message.builder()
+                        .content(userText)
+                        .role("user")
+                        .build()
+        );
+
         ChatCompletionRequest request = ChatCompletionRequest.builder()
                 .model("gpt-3.5-turbo")
-                .messages(List.of(
-                        Message.builder()
-                                .content(userText)
-                                .role("user")
-                                .build()
-                ))
+                .messages(gptChatHistory.getChatHistory())
                 .maxTokens(100) // set limit for tokens
                 .build()
         ;
 
         ChatCompletion response = gptClient.createChatCompletion(request);
+        Message messageFromGpt = response.getChoises().getFirst().getMessage();
+        gptChatHistoryService.updateChatIdHistory(userChatId, messageFromGpt);
 
         return response.getChoises().getFirst().getMessage().getContent();
     }
