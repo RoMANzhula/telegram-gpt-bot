a739d4bbad37bb485cd1f0920c9c6e30adbb8cd3
RoMANzhula <roman1985al@ukr.net>
2025-01-22 23:23:34 +0200

    Add StartCommandHandler

---
+++ src/main/java/org/romanzhula/telegram_gpt_bot/telegram/commands/handlers/StartCommandHandler.java
@@ -0,0 +1,35 @@
+package org.romanzhula.telegram_gpt_bot.telegram.commands.handlers;
+
+import org.romanzhula.telegram_gpt_bot.telegram.commands.enums.TelegramCommands;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+@Component
+public class StartCommandHandler implements TelegramCommandHandler {
+
+    private final String GREETING_MESSAGE = """
+            Welcome %s!
+            This Bot is for your Chat GPT.
+            Write your question and you'll receive an answer within a few seconds.
+            Watch all command you can via command '/help'
+            """
+    ;
+
+    @Override
+    public TelegramCommands getSupportedCommand() {
+        return TelegramCommands.START_COMMAND;
+    }
+
+    @Override
+    public BotApiMethod<?> processCommand(Update update) {
+        return SendMessage.builder()
+                .chatId(update.getMessage().getChatId())
+                .text(GREETING_MESSAGE.formatted(update.getMessage().getChat().getFirstName()))
+                .build()
+        ;
+    }
+
+}
