eabb7e82f664702978ac95d544685f6de42e6dce
RoMANzhula <roman1985al@ukr.net>
2025-01-23 22:51:21 +0200

    Up GptClient - for convert audio file to text message

--- src/main/java/org/romanzhula/telegram_gpt_bot/gpt_openai/GptClient.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/gpt_openai/GptClient.java
@@ -2,10 +2,13 @@ package org.romanzhula.telegram_gpt_bot.gpt_openai;
 
 import org.romanzhula.telegram_gpt_bot.gpt_openai.models.ChatCompletion;
 import org.romanzhula.telegram_gpt_bot.gpt_openai.requests.ChatCompletionRequest;
+import org.romanzhula.telegram_gpt_bot.gpt_openai.requests.TranslationRequest;
+import org.romanzhula.telegram_gpt_bot.gpt_openai.responses.TranscriptionResponse;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
 
 
 @Component
@@ -37,6 +40,31 @@ public class GptClient {
                 HttpMethod.POST,
                 httpEntity,
                 ChatCompletion.class
+        );
+
+        return responseEntity.getBody();
+    }
+
+    public TranscriptionResponse createTranslation(
+        TranslationRequest translationRequest
+    ) {
+        String url = "https://api.openai.com/v1/audio/translations";
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization", "Bearer " + token);
+        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
+        bodyMap.add("file", translationRequest.getAudioFile());
+        bodyMap.add("model", translationRequest.getModel());
+
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(bodyMap, httpHeaders);
+
+        ResponseEntity<TranscriptionResponse> responseEntity = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                httpEntity,
+                TranscriptionResponse.class
         );
 
         return responseEntity.getBody();
