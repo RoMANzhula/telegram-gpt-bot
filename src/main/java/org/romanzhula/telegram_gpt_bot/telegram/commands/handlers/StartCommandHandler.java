d6423cc6ae6c0d0b4cf92c3065b0c966961d00e3
RoMANzhula <roman1985al@ukr.net>
2025-01-25 22:28:50 +0200

    Add StartCommandHandler - optimize code

--- src/main/java/org/romanzhula/telegram_gpt_bot/telegram/commands/handlers/StartCommandHandler.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/telegram/commands/handlers/StartCommandHandler.java
@@ -4,7 +4,7 @@ import org.romanzhula.telegram_gpt_bot.telegram.commands.enums.TelegramCommands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.Message;
 
 
 @Component
@@ -24,10 +24,10 @@ public class StartCommandHandler implements TelegramCommandHandler {
     }
 
     @Override
-    public BotApiMethod<?> processCommand(Update update) {
+    public BotApiMethod<?> processCommand(Message message) {
         return SendMessage.builder()
-                .chatId(update.getMessage().getChatId())
-                .text(GREETING_MESSAGE.formatted(update.getMessage().getChat().getFirstName()))
+                .chatId(message.getChatId())
+                .text(GREETING_MESSAGE.formatted(message.getChat().getFirstName()))
                 .build()
         ;
     }
