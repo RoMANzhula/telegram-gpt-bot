b0438d77ae676876a93e087b1e78f96fa74f8105
RoMANzhula <roman1985al@ukr.net>
2025-01-25 22:03:21 +0200

    Add TelegramMessageUpdateHandlerService - optimize code

---
+++ src/main/java/org/romanzhula/telegram_gpt_bot/telegram/commands/services/TelegramMessageUpdateHandlerService.java
@@ -0,0 +1,62 @@
+package org.romanzhula.telegram_gpt_bot.telegram.commands.services;
+
+import lombok.RequiredArgsConstructor;
+import org.romanzhula.telegram_gpt_bot.telegram.async.TelegramAsyncMessageSenderService;
+import org.romanzhula.telegram_gpt_bot.telegram.commands.handlers.messages.TelegramCommandMessageHandler;
+import org.romanzhula.telegram_gpt_bot.telegram.commands.handlers.messages.TelegramTextMessageHandler;
+import org.romanzhula.telegram_gpt_bot.telegram.commands.handlers.messages.TelegramVoiceMessageHandler;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class TelegramMessageUpdateHandlerService {
+
+    private final TelegramCommandMessageHandler commandMessageHandler;
+    private final TelegramTextMessageHandler textMessageHandler;
+    private final TelegramVoiceMessageHandler telegramVoiceMessageHandler;
+    private final TelegramAsyncMessageSenderService asyncMessageSender;
+
+
+    public List<BotApiMethod<?>> handleUpdate(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            Long userChatId = message.getChatId();
+
+            if (commandMessageHandler.isCommand(message)) {
+                return commandMessageHandler.handleCommand(message);
+            }
+
+            if (message.hasVoice()) {
+                asyncMessageSender.sendMessageAsync(
+                        userChatId.toString(),
+                        () -> (SendMessage) telegramVoiceMessageHandler.handleVoiceMessage(message),
+                        throwable -> new SendMessage(
+                                userChatId.toString(),
+                                "Error processing voice message."
+                        )
+                );
+            }
+
+            if (message.hasText()) {
+                asyncMessageSender.sendMessageAsync(
+                        userChatId.toString(),
+                        () -> (SendMessage) textMessageHandler.handleTextMessage(message),
+                        throwable -> new SendMessage(
+                                userChatId.toString(),
+                                "Error processing text message."
+                        )
+                );
+            }
+        }
+
+        return List.of();
+    }
+
+}
