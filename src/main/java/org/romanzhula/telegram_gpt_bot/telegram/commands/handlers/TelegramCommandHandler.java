d7f8344853729d0cd32a04270cec52b812e905c9
RoMANzhula <roman1985al@ukr.net>
2025-01-25 22:07:45 +0200

    Up TelegramCommandHandler - optimize code

--- src/main/java/org/romanzhula/telegram_gpt_bot/telegram/commands/handlers/TelegramCommandHandler.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/telegram/commands/handlers/TelegramCommandHandler.java
@@ -2,12 +2,12 @@ package org.romanzhula.telegram_gpt_bot.telegram.commands.handlers;
 
 import org.romanzhula.telegram_gpt_bot.telegram.commands.enums.TelegramCommands;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.Message;
 
 public interface TelegramCommandHandler {
 
     TelegramCommands getSupportedCommand();
 
-    BotApiMethod<?> processCommand(Update update);
+    BotApiMethod<?> processCommand(Message message);
 
 }
