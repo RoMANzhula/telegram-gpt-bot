1ee7c04be831a7a2d1f9b3502a76db0b73023ac8
RoMANzhula <roman1985al@ukr.net>
2025-01-25 22:22:09 +0200

    Add ErrorMessageBuilder - optimize code

---
+++ src/main/java/org/romanzhula/telegram_gpt_bot/telegram/helpers/ErrorMessageBuilder.java
@@ -0,0 +1,18 @@
+package org.romanzhula.telegram_gpt_bot.telegram.helpers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+@Slf4j
+public class ErrorMessageBuilder {
+
+    public static SendMessage buildErrorMessage(
+            String chatId, String errorMessage, Throwable throwable) {
+        log.error(errorMessage, throwable);
+        return SendMessage.builder()
+                .chatId(chatId)
+                .text(errorMessage)
+                .build();
+    }
+
+}
