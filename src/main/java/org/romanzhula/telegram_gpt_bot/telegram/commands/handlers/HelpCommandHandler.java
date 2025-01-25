24e3ce579dc214dd7072f14f64635b344b2a03f1
RoMANzhula <roman1985al@ukr.net>
2025-01-25 22:29:19 +0200

    Add HelpCommandHandler - optimize code

--- src/main/java/org/romanzhula/telegram_gpt_bot/telegram/commands/handlers/HelpCommandHandler.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/telegram/commands/handlers/HelpCommandHandler.java
@@ -4,7 +4,7 @@ import org.romanzhula.telegram_gpt_bot.telegram.commands.enums.TelegramCommands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.Message;
 
 @Component
 public class HelpCommandHandler implements TelegramCommandHandler {
@@ -22,9 +22,9 @@ public class HelpCommandHandler implements TelegramCommandHandler {
     }
 
     @Override
-    public BotApiMethod<?> processCommand(Update update) {
+    public BotApiMethod<?> processCommand(Message message) {
         return SendMessage.builder()
-                .chatId(update.getMessage().getChatId())
+                .chatId(message.getChatId())
                 .text(HELP_MENU_MESSAGE)
                 .build()
         ;
