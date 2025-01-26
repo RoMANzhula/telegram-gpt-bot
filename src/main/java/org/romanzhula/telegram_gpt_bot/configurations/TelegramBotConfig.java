a59236e2056f6c84a5c5756f1fa9c8847a610181
RoMANzhula <roman1985al@ukr.net>
2025-01-26 21:26:27 +0200

    Up TelegramBotConfig - optimize code

--- src/main/java/org/romanzhula/telegram_gpt_bot/configurations/TelegramBotConfig.java
+++ src/main/java/org/romanzhula/telegram_gpt_bot/configurations/TelegramBotConfig.java
@@ -1,5 +1,6 @@
 package org.romanzhula.telegram_gpt_bot.configurations;
 
+import lombok.extern.slf4j.Slf4j;
 import org.romanzhula.telegram_gpt_bot.telegram.TelegramBot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 
+@Slf4j
 @Configuration
 public class TelegramBotConfig {
 
@@ -19,6 +21,7 @@ public class TelegramBotConfig {
             telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(telegramBot);
         } catch (TelegramApiException e) {
+            log.error("The Bot: {}, was not registered.", telegramBot.getBotUsername());
             throw new RuntimeException(e);
         }
 
